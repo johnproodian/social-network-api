d472a7cc5af65d6362ae54d803b3d65e56f26f67
Jotas <jproodian721@gmail.com>
2022-04-16 15:59:54 -0500

    set up file structure and some comments to map out models, routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,10 @@
+// /api/thoughts
+    // Get all thoughts
+    // Get a single thought by _id
+    // Post to create new thought (incl. pushing the created thought's _id to the associated user's thoughts array field)
+    // Put to update a thought by its _id
+    // Delete a thought by its _id
+
+// /api/thoughts/:thoughtId/reactions
+    // Post to create reaction stored in a single thought's reactions array field
+    // Delete to pull and remove a reaction by the raction's reactionId
---
+++ routes/api/user-routes.js
@@ -0,0 +1,14 @@
+
+// /api/users
+    // Get all users
+    // Get a single user by _id and populated thought and friend data
+    // Post a new user
+    // Put to update a user by its _id
+    // Delete to remove a user by its _id
+    // BONUS: Remove a user's associated thoughts when deleted
+
+
+// /api/users/:userId/friends/:friendId
+    // Post to add a new friend to a user's friend list
+    // Delete to remove a friend from a user's friend list
+    
